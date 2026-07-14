ee2855f8773246234640ade6b3950f8f2ae2a236
5omeOtherGuy <268151429+5omeOtherGuy@users.noreply.github.com>
2026-07-14 11:19:41 +0200

    Revert "test: add live cross-session cache isolation probes"
    This reverts commit a9fa787a5549bb7267cd8a0ba499706347ce2eaa.

--- src/mimir/providers/mod.rs
+++ src/mimir/providers/mod.rs
@@ -14,9 +14,6 @@ pub(crate) mod openai_codex_responses;
 pub(crate) mod openai_compatible_chat;
 mod transport;
 
-#[cfg(test)]
-mod prompt_cache_live_tests;
-
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use std::sync::Mutex;
